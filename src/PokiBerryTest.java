fd30b26c6d6fc47b7585c31cffdcbbe3e1b12cc1
Zachary Zensen <TechnomagezZ@yahoo.com>
2020-06-02 19:20:18 -0700

    Tests not yet completed

---
+++ src/PokiBerryTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import junit.framework.TestCase;
+
+class PokiBerryTest extends TestCase{
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
